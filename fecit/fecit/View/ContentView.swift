5e18a3d5d2da8cb9c5ddd3fb7e106b14847d4872
nayeon <nayeon7612@gmail.com>
2024-04-18 20:00:18 +0900

    Feat: 프로젝트 생성

---
+++ fecit/fecit/View/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  fecit
+//
+//  Created by nayeon  on 4/18/24.
+//
+
+import SwiftUI
+
+public struct ContentView: View {
+    
+    public init() { }
+    
+    public var body: some View {
+        Text("Hello World")
+    }
+    
+}
+
+#Preview {
+    ContentView()
+}
---
+++ fecit/fecit/fecitApp.swift
@@ -0,0 +1,17 @@
+//
+//  fecitApp.swift
+//  fecit
+//
+//  Created by nayeon  on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct fecitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
