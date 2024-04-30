31bbcde489e3bab7e4b85fcab07ee4e3dbb1fd3c
angryeon7 <nayeon7612@gmail.com>
2024-04-30 14:30:55 +0900

    Feat: 뷰모델 연결 및 스크롤 기능 수정

--- fecit/fecit/View/ActionView.swift
+++ fecit/fecit/View/ActionView.swift
@@ -7,46 +7,60 @@
 
 import SwiftUI
 
-//TODO: 스크롤 변경
-struct ActionView: View {
-    @State private var currentPage = 0
-    @State private var months: [Date] = []
-    @State private var currentDate = Date()
-
-    var body: some View {
-        VStack {
+public struct ActionView: View {
+    @ObservedObject public var viewModel: CalendarViewModel
+    @State private var offsetY: CGFloat = 0
+    
+    var informations = [YearMonthDay: [(String, Color)]]()
+    
+    public var body: some View {
+        ZStack(alignment: .bottom) {
             VStack {
-                CalendarHeaderView(currentDate: $currentDate, addWeek: false)
-                    .padding(10)
-                WeekdayView()
-            }
-            .background(Color("MainBlue"))
-            Spacer()
-            ScrollView(.vertical, showsIndicators: false) {
-                ScrollViewReader { scrollView in
-                    VStack(spacing: 0) {
-                        ForEach(0..<12) { index in
-                            let month = Calendar.current.date(byAdding: .month, value: index, to: currentDate)!
-                            CalendarGridView(currentDate: .constant(month))
-                                .id(index)
-                                .frame(height: UIScreen.main.bounds.height * 0.96) 
+                VStack {
+                    CalendarHeaderView(viewModel: viewModel)
+                        .padding(10)
+                    WeekdayView()
+                }
+                .background(Color("MainBlue"))
+                
+                ScrollViewReader { scrollViewProxy in
+                    ScrollView(.vertical, showsIndicators: false) {
+                        VStack {
+                            CalendarGridView(weeks: viewModel.current.weeks, informations: informations)
+                                .frame(maxWidth: .infinity)
+                                .animation(.default)
+                                .id("MonthView")
                         }
                     }
-                    .frame(maxWidth: .infinity)
-                    .offset(y: CGFloat(currentPage) * -UIScreen.main.bounds.height)
                     .onAppear {
-                        let currentMonth = Calendar.current.component(.month, from: currentDate)
-                        currentPage = currentMonth - 1
-                        scrollView.scrollTo(currentPage)
+                        scrollViewProxy.scrollTo("MonthView", anchor: .top)
                     }
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                let translation = value.translation.height
+                                if offsetY <= 10 && translation > 20 {
+                                    viewModel.previousPage()
+                                } else if offsetY >= value.translation.height && translation < -20 {
+                                    viewModel.nextPage()
+                                }
+                            }
+                    )
                 }
             }
-            Spacer()
         }
-        .coordinateSpace(name: "scroll")
+    }
+
+    public init(viewModel: CalendarViewModel) {
+        self.viewModel = viewModel
     }
 }
 
-#Preview {
-    ActionView()
+public struct ActionView_Previews: PreviewProvider {
+    static public var previews: some View {
+        let dayTimeInterval: TimeInterval = 24 * 60 * 60
+        let viewModel = CalendarViewModel(heighlighted: [.init(timeIntervalSinceNow: dayTimeInterval)])
+        return ActionView(viewModel: viewModel)
+            .previewLayout(.sizeThatFits)
+    }
 }
