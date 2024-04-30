4d39856d841976767848063990403be8134d1c97
angryeon7 <nayeon7612@gmail.com>
2024-04-30 13:55:31 +0900

    Feat: CalendarViewModel 구현

---
+++ fecit/fecit/View/CalendarViewModel.swift
@@ -0,0 +1,83 @@
+//
+//  CalendarViewModel.swift
+//  fecit
+//
+//  Created by nayeon  on 4/29/24.
+//
+
+import SwiftUI
+
+final public class CalendarViewModel: ObservableObject {
+    @Published public var selectedDate: Date?
+    @Published var current: Page
+    
+    private(set) lazy var weekdaySymbols: [String] = DateFormatter().shortWeekdaySymbols
+
+    public init(
+        today: Date = .init(),
+        heighlighted: [Date] = .init(),
+        selectedDate: Date? = nil
+    ) {
+        self.heighlighted = heighlighted
+        self.today = today
+        self.current = Page(
+            month: today.month,
+            year: today.year,
+            heighlighted: heighlighted,
+            selected: selectedDate
+        )
+        self.current.select = update(date:)
+    }
+
+    private let heighlighted: [Date]
+    private let today: Date
+
+    func nextPage() {
+        if current.month == 12 {
+            reloadPage(
+                month: 1,
+                year: current.year + 1
+            )
+        } else {
+            reloadPage(
+                month: current.month + 1,
+                year: current.year
+            )
+        }
+    }
+
+    func previousPage() {
+        if current.month == 1 {
+            reloadPage(
+                month: 12,
+                year: current.year - 1
+            )
+        } else {
+            reloadPage(
+                month: current.month - 1,
+                year: current.year
+            )
+        }
+    }
+
+    private func update(date: Date) {
+        selectedDate = date
+        reloadPage(
+            month: current.month,
+            year: current.year
+        )
+    }
+
+    private func reloadPage(
+        month: Int,
+        year: Int
+    ) {
+        current = Page(
+            month: month,
+            year: year,
+            heighlighted: heighlighted,
+            selected: selectedDate,
+            select: update(date:)
+        )
+    }
+}
